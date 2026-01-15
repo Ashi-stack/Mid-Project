86ed69a0e584248ec0270af463dc5c810fa35a25
Ashi-stack <ayeshaabdulrazzaq130@gmail.com>
2026-01-15 06:15:11 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// JavaScript for EmpowerHer Website
+// This file contains basic JavaScript concepts taught in class
+
+// Display a welcome message to the user
+alert("Welcome to EmpowerHer! Together, we can create awareness and change.");
+
+// Print a message in the browser console
+console.log("EmpowerHer website loaded successfully");
+
+// Storing information in variables
+var websiteName = "EmpowerHer";
+var launchYear = 2026;
+
+// Printing variable values
+console.log("Website Name:", websiteName);
+console.log("Launch Year:", launchYear);
+
+// Checking data types
+console.log("Type of websiteName:", typeof websiteName);
+console.log("Type of launchYear:", typeof launchYear);
+
+// Example of an array
+var values = ["Awareness", "Education", "Support"];
+
+// Checking if a variable is an array
+console.log("Is values an array?", Array.isArray(values));
+
+/*
+This JavaScript file demonstrates:
+- alert()
+- console.log()
+- variables
+- typeof operator
+- Array.isArray()
+- single-line and multi-line comments
+*/
